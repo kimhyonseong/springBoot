59684c3a0c012ab044103c4c1c8cff5f0af80c33
kimhyonseong <corea6524@gmail.com>
2022-08-30 00:00:16 +0900

    file upload

--- food-list/src/main/java/com/example/foodlist/restController/ImgRestApiController.java
+++ food-list/src/main/java/com/example/foodlist/restController/ImgRestApiController.java
@@ -1,31 +1,24 @@
 package com.example.foodlist.restController;
 
 import com.example.foodlist.support.FileUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.ServletContext;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 @RestController
+@RequiredArgsConstructor
 public class ImgRestApiController {
-//    @PostMapping("rest/foodImgUpload")
-//    public ResponseEntity<?> imgUpload(
-//            //@Valid @RequestParam("file") MultipartFile file
-//            MultipartHttpServletRequest multipartHttpServletRequest
-//            ) throws Exception{
-//        MultipartFile file = multipartHttpServletRequest.getFile("file");
-//        System.out.println("------------------------------");
-//        System.out.println(multipartHttpServletRequest);
-//        System.out.println(multipartHttpServletRequest.getFile("file"));
-//        assert file != null;
-//        String result = "img_url : "+file.getOriginalFilename();
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(result);
-//    }
+    private final ServletContext servletContext;
 
     @PostMapping("rest/foodImgUpload")
     public ResponseEntity<?> imgUpload(
@@ -35,9 +28,22 @@ public class ImgRestApiController {
         boolean isImg = fileUtils.isImgMimeType(file);
 
         long fileSize = file.getSize();
+        final String UPLOAD_PATH = "C:\\Users\\khs13\\Desktop\\foodListSpringBoot\\food-list\\src\\main\\resources\\static\\image\\tmp";
+        final String webRoot = servletContext.getRealPath("/");  // webapp으로 설정되어있어 사용하면 안됨
         System.out.println("file origin name : "+file.getOriginalFilename());
         System.out.println("isImg : "+isImg);
         System.out.println("file size : "+fileSize);
+        System.out.println("webRoot : "+webRoot);
+
+        if(isImg) {
+            try {
+                File newFile = new File(UPLOAD_PATH, Objects.requireNonNull(file.getOriginalFilename()));
+                file.transferTo(newFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(file.getOriginalFilename());
     }
