e2f33a857e4250747dc37d6c80f2a8565654be49
kimhyonseong <corea6524@gmail.com>
2022-08-30 23:59:51 +0900

    create MvcConfig file

---
+++ food-list/src/main/java/com/example/foodlist/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.foodlist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    private final Path FILE_ROOT = Paths.get("./images").toAbsolutePath().normalize();
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/images/tmp")
+                .addResourceLocations(FILE_ROOT.toUri().toString());
+    }
+}
