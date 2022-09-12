2924b3a94ade01be681ad1548736f6499591103a
kimhyonseong <corea6524@gmail.com>
2022-09-12 22:57:31 +0900

    remove table food_review_info, create query avg score

--- food-list/src/main/java/com/example/foodlist/repository/FoodReviewInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.foodlist.repository;
-
-import com.example.foodlist.domain.FoodReviewInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodReviewInfoRepository extends JpaRepository<FoodReviewInfo,Long> {
-}
--- food-list/src/main/java/com/example/foodlist/repository/ReviewRepository.java
+++ food-list/src/main/java/com/example/foodlist/repository/ReviewRepository.java
@@ -3,10 +3,16 @@ package com.example.foodlist.repository;
 import com.example.foodlist.domain.Food;
 import com.example.foodlist.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
     List<Review> findAllByFood(Food food);
     Review findByFoodIdxAndMemberId(Long foodIdx,String memberId);
+
+    @Query(value = "select avg(score) over(partition by food_idx) result from review " +
+            "where food_idx = :foodIdx limit 1", nativeQuery = true)
+    Long avgScore(@Param("foodIdx") Long foodIdx);
 }
