466ba2b5e8bf536af10967e87b22302cbe14c24f
kimhyonseong <corea6524@gmail.com>
2022-10-27 18:02:49 +0900

    create springboot

---
+++ foodPreference/src/main/java/com/example/foodpreference/FoodPreferenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodpreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPreferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodPreferenceApplication.class, args);
+    }
+
+}
---
+++ foodPreference/src/test/java/com/example/foodpreference/FoodPreferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodpreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPreferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
